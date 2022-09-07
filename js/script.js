d6a28ef4f13204ede1de87f8dd7e49e4a288bd92
Hélène Hassan <helenehhassan@gmail.com>
2022-09-07 20:55:01 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ var menu = document.getElementById("menu");
 menu.style.maxHeight = "0%";
 
 function togglemenu() {
-    console.log('toggle menu');
     if (menu.style.maxHeight == "0%") {   
         menu.style.maxHeight = "50%";
     }
@@ -12,3 +11,15 @@ function togglemenu() {
     }
 }
 
+var CV_menu = document.getElementById("CV_menu");
+
+CV_menu.style.maxHeight = "0%";
+
+function toggleCV() {
+    if (CV_menu.style.maxHeight == "0%") {
+        CV_menu.style.maxHeight = "10%";
+    }
+    else {
+        CV_menu.style.maxHeight = "0%";
+    }
+}
