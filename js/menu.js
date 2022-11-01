b8392dce172e19126bb5e9ca0a57b31c253f319e
helenhsn <helenehhassan@gmail.com>
2022-11-01 11:19:33 +0100

    Modifications : navbar position fixed + menu bug corrected + some other bugs

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ menu.style.maxHeight = "0%";
 
 function togglemenu() {
     if (menu.style.maxHeight == "0%") {   
-        menu.style.maxHeight = "50%";
+        menu.style.maxHeight = "15rem";
     }
     else {
         menu.style.maxHeight = "0%";
