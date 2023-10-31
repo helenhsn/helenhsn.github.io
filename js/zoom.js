e84f18ca23b225f471aa818d71d8098be072568d
helenhsn <mimihassan34@gmail.com>
2023-10-31 17:04:50 +0100

    final touch

--- js/zoom.js
+++ js/zoom.js
@@ -8,10 +8,7 @@ $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
         $('.contents').off('keyup.modal-close');
     }
     let newWidth;
-    let factor = 2;
-    if (window.matchMedia("(max-width:700px").matches || this.id=='raycaster') {
-        factor = 1.10;
-    }
+    let factor = 1;
     newWidth = Math.min(window.innerWidth, window.innerHeight)/factor;
     bgSize = newWidth.toString() + 'px ' + ' auto' ;
 
