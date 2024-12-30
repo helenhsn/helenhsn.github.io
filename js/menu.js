dba3521cb6366ceaf27315112e14ddf9b8510887
helenhsn <mimihassan34@gmail.com>
2024-12-30 09:42:04 +0100

    png -> jpg & test on mobile

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,7 @@ function togglemenu() {
 
     if (menu.style.maxHeight == "0%") {
         menu.style.maxHeight = "18rem";
+        menu.style.backgroundColor = "rgba(0, 0, 0, 1.0)";
     }
     else {
         menu.style.maxHeight = "0%";
